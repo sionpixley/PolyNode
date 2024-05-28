9c2dac11e02f6c3612dd8e2caeff21fe9d6ec59e
Sion Pixley <sionpixley@gmail.com>
2024-05-27 21:50:42 -0500

    Getting the setup executable to work on Linux

--- install/setup.go
+++ install/setup.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
 	"os/exec"
 	"runtime"
 )
+
+const c_FILE_CONTENT string = `#!/bin/bash
+
+if [[ ":$PATH:" != *":/opt/PolyNode:"* ]]; then
+    PATH=$PATH:/opt/PolyNode
+fi`
 
 func main() {
 	operatingSystem := runtime.GOOS
@@ -14,40 +22,13 @@ func main() {
 	var err error
 	switch operatingSystem {
 	case "darwin":
+		err = installMac()
 	case "linux":
-		err = exec.Command("sudo", "cp", "-r", "PolyNode", "/opt").Run()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		err = exec.Command("sudo", "touch", "/etc/profile.d/polyn-path.sh").Run()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		err = exec.Command("bash", "-c", "echo export PATH=\\$PATH:/opt/PolyNode | sudo tee /etc/profile.d/polyn-path.sh").Run()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		err = exec.Command("sudo", "chmod", "+x", "/etc/profile.d/polyn-path.sh").Run()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-
-		err = exec.Command("sudo", "/etc/profile.d/polyn-path.sh").Run()
+		err = installLinux()
 	case "windows":
-		err = exec.Command("cmd.exe", "/c", `cd "C:\\Program Files" && mkdir polyn`).Run()
-		if err != nil {
-			fmt.Println(err.Error())
-			return
-		}
-		err = exec.Command("robocopy", ".\\polyn", "C:\\Program Files\\polyn").Run()
+		err = installWindows()
 	default:
+		err = errors.New("unsupported operating system")
 	}
 
 	if err != nil {
@@ -55,6 +36,29 @@ func main() {
 	} else {
 		fmt.Println("The polyn command has been installed. Please close all open terminals.")
 	}
+}
+
+func installLinux() error {
+	err := exec.Command("sudo", "cp", "-r", "PolyNode", "/opt").Run()
+	if err != nil {
+		return err
+	}
+
+	err = os.WriteFile("/etc/profile.d/polyn-path.sh", []byte(c_FILE_CONTENT), 0755)
+	if err != nil {
+		return err
+	}
+
+	err = exec.Command("bash", "-c", "source /etc/profile.d/polyn-path.sh").Run()
+	return err
+}
+
+func installMac() error {
+	return nil
+}
+
+func installWindows() error {
+	return nil
 }
 
 func printOptionalLine(operatingSystem string) {
