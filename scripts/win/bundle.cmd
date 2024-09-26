628b311942d2ed273124f32e01486b6f678c6671
Sion Pixley <sionpixley@gmail.com>
2024-09-26 17:40:03 -0500

    Changes to bundle batchfile to catch up to rc.4

--- scripts/win/bundle.cmd
+++ scripts/win/bundle.cmd
@@ -1,36 +1,28 @@
 @echo off
 
-set version=v0.8.0-rc.3
+set version=v0.8.0-rc.4
 
 :: Build Windows ARM64
 
 set GOOS=windows
 set GOARCH=arm64
 
-go build -tags=prod -o polyn-arm64.exe ./cmd/polyn
+go build -tags=prod -o polyn.exe ./cmd/polyn
 
 cd install
-go build -o ../setup-arm64.exe ./cmd/setup
+go build -o ../setup.exe ./cmd/setup
 
 cd ..\uninstall
 go build -o ../uninstall-arm64.exe
 
-cd ..\web
-go build -tags=prod -o ../PolyNode-arm64.exe ./cmd/serve
-
-cd gui
-call pnpm install
-call pnpm run build
-
-cd ..\..
+cd ..
 mkdir PolyNode-%version%-win-arm64
-move setup-arm64.exe .\PolyNode-%version%-win-arm64\setup.exe
+move setup.exe .\PolyNode-%version%-win-arm64\setup.exe
 
 cd PolyNode-%version%-win-arm64
 mkdir PolyNode
 cd ..
-move polyn-arm64.exe .\PolyNode-%version%-win-arm64\PolyNode\polyn.exe
-move PolyNode-arm64.exe .\PolyNode-%version%-win-arm64\PolyNode\PolyNode.exe
+copy polyn.exe .\PolyNode-%version%-win-arm64\PolyNode
 
 copy README.md .\PolyNode-%version%-win-arm64\PolyNode
 copy LICENSE .\PolyNode-%version%-win-arm64\PolyNode
@@ -38,36 +30,66 @@ copy SECURITY.md .\PolyNode-%version%-win-arm64\PolyNode
 
 cd .\PolyNode-%version%-win-arm64\PolyNode
 mkdir uninstall
+cd ..\..
+copy uninstall-arm64.exe .\PolyNode-%version%-win-arm64\PolyNode\uninstall
+cd .\PolyNode-%version%-win-arm64\PolyNode\uninstall
+ren uninstall-arm64.exe uninstall.exe
+cd ..\..\..
+
+:: Build Windows ARM64 gui
+
+cd install
+go build -tags=gui -o ../setup.exe ./cmd/setup
+
+cd ..\web
+go build -tags=prod -o ../PolyNode.exe ./cmd/serve
+
+cd gui
+call pnpm install
+call pnpm run build
+
+cd ..\..
+mkdir PolyNode-GUI-%version%-win-arm64
+move setup.exe .\PolyNode-GUI-%version%-win-arm64\setup.exe
+
+cd PolyNode-GUI-%version%-win-arm64
+mkdir PolyNode
+cd ..
+move polyn.exe .\PolyNode-GUI-%version%-win-arm64\PolyNode\polyn.exe
+move PolyNode.exe .\PolyNode-GUI-%version%-win-arm64\PolyNode\PolyNode.exe
+
+copy README.md .\PolyNode-GUI-%version%-win-arm64\PolyNode
+copy LICENSE .\PolyNode-GUI-%version%-win-arm64\PolyNode
+copy SECURITY.md .\PolyNode-GUI-%version%-win-arm64\PolyNode
+
+cd .\PolyNode-GUI-%version%-win-arm64\PolyNode
+mkdir uninstall
 mkdir gui
 cd ..\..
-move uninstall-arm64.exe .\PolyNode-%version%-win-arm64\PolyNode\uninstall\uninstall.exe
-xcopy /s /i .\web\gui\dist\ .\PolyNode-%version%-win-arm64\PolyNode\gui\dist\
+move uninstall-arm64.exe .\PolyNode-GUI-%version%-win-arm64\PolyNode\uninstall\uninstall.exe
+xcopy /s /i .\web\gui\dist\ .\PolyNode-GUI-%version%-win-arm64\PolyNode\gui\dist\
 
 :: Build Windows x64
 
 set GOOS=windows
 set GOARCH=amd64
 
-go build -tags=prod -o polyn-x64.exe ./cmd/polyn
+go build -tags=prod -o polyn.exe ./cmd/polyn
 
 cd install
-go build -o ../setup-x64.exe ./cmd/setup
+go build -o ../setup.exe ./cmd/setup
 
 cd ..\uninstall
 go build -o ../uninstall-x64.exe
 
-cd ..\web
-go build -tags=prod -o ../PolyNode-x64.exe ./cmd/serve
-
 cd ..
 mkdir PolyNode-%version%-win-x64
-move setup-x64.exe .\PolyNode-%version%-win-x64\setup.exe
+move setup.exe .\PolyNode-%version%-win-x64\setup.exe
 
 cd PolyNode-%version%-win-x64
 mkdir PolyNode
 cd ..
-move polyn-x64.exe .\PolyNode-%version%-win-x64\PolyNode\polyn.exe
-move PolyNode-x64.exe .\PolyNode-%version%-win-x64\PolyNode\PolyNode.exe
+copy polyn.exe .\PolyNode-%version%-win-x64\PolyNode
 
 copy README.md .\PolyNode-%version%-win-x64\PolyNode
 copy LICENSE .\PolyNode-%version%-win-x64\PolyNode
@@ -75,10 +97,40 @@ copy SECURITY.md .\PolyNode-%version%-win-x64\PolyNode
 
 cd .\PolyNode-%version%-win-x64\PolyNode
 mkdir uninstall
+cd ..\..
+copy uninstall-x64.exe .\PolyNode-%version%-win-x64\PolyNode\uninstall
+cd .\PolyNode-%version%-win-x64\PolyNode\uninstall
+ren uninstall-x64.exe uninstall.exe
+cd ..\..\..
+
+:: build Windows x64 gui
+
+cd install
+go build -tags=gui -o ../setup.exe ./cmd/setup
+
+cd ..\web
+go build -tags=prod -o ../PolyNode.exe ./cmd/serve
+
+cd ..
+mkdir PolyNode-GUI-%version%-win-x64
+move setup.exe .\PolyNode-GUI-%version%-win-x64\setup.exe
+
+cd PolyNode-GUI-%version%-win-x64
+mkdir PolyNode
+cd ..
+move polyn.exe .\PolyNode-GUI-%version%-win-x64\PolyNode\polyn.exe
+move PolyNode.exe .\PolyNode-GUI-%version%-win-x64\PolyNode\PolyNode.exe
+
+copy README.md .\PolyNode-GUI-%version%-win-x64\PolyNode
+copy LICENSE .\PolyNode-GUI-%version%-win-x64\PolyNode
+copy SECURITY.md .\PolyNode-GUI-%version%-win-x64\PolyNode
+
+cd .\PolyNode-GUI-%version%-win-x64\PolyNode
+mkdir uninstall
 mkdir gui
 cd ..\..
-move uninstall-x64.exe .\PolyNode-%version%-win-x64\PolyNode\uninstall\uninstall.exe
-xcopy /s /i .\web\gui\dist\ .\PolyNode-%version%-win-x64\PolyNode\gui\dist\
+move uninstall-x64.exe .\PolyNode-GUI-%version%-win-x64\PolyNode\uninstall\uninstall.exe
+xcopy /s /i .\web\gui\dist\ .\PolyNode-GUI-%version%-win-x64\PolyNode\gui\dist\
 
 :: Bundle
 
@@ -86,6 +138,14 @@ powershell -Command "Compress-Archive -Path PolyNode-%version%-win-arm64 -Destin
 del PolyNode-%version%-win-arm64 /s /f /q > nul
 rmdir PolyNode-%version%-win-arm64 /s /q
 
+powershell -Command "Compress-Archive -Path PolyNode-GUI-%version%-win-arm64 -DestinationPath PolyNode-GUI-%version%-win-arm64.zip"
+del PolyNode-GUI-%version%-win-arm64 /s /f /q > nul
+rmdir PolyNode-GUI-%version%-win-arm64 /s /q
+
 powershell -Command "Compress-Archive -Path PolyNode-%version%-win-x64 -DestinationPath PolyNode-%version%-win-x64.zip"
 del PolyNode-%version%-win-x64 /s /f /q > nul
 rmdir PolyNode-%version%-win-x64 /s /q
+
+powershell -Command "Compress-Archive -Path PolyNode-GUI-%version%-win-x64 -DestinationPath PolyNode-GUI-%version%-win-x64.zip"
+del PolyNode-GUI-%version%-win-x64 /s /f /q > nul
+rmdir PolyNode-GUI-%version%-win-x64 /s /q
