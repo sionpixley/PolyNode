493f5b895872dbecc7b4e37a875e896f86cfe574
Sion Pixley <sionpixley@gmail.com>
2024-09-29 19:35:45 -0500

    I guess its time for the official release

--- scripts/win/bundle.cmd
+++ scripts/win/bundle.cmd
@@ -1,6 +1,6 @@
 @echo off
 
-set version=v0.8.0-rc.7
+set version=v0.8.0
 
 :: Build Windows ARM64
 
--- web/gui/src/app/components/available/available.component.spec.ts
+++ web/gui/src/app/components/available/available.component.spec.ts
@@ -18,35 +18,4 @@ describe('AvailableComponent', () => {
     let expected = ['v20.22.1', 'v18.11.3', 'v18.2.1', 'v10.12.1', 'v8.19.1'];
     expect(component.availableVersions).toStrictEqual(expected);
   });
-
-  it('should select lts versions correctly', () => {
-    component.availableVersions = [
-      'v22.9.0',
-      'v21.7.3',
-      'v19.9.0',
-      'v17.9.1',
-      'v15.14.0',
-      'v13.14.0',
-      'v11.15.0',
-      'v20.17.0',
-      'v18.20.4',
-      'v16.20.2',
-      'v14.21.3',
-      'v12.22.12',
-      'v10.24.1',
-      'v8.17.0'
-    ];
-    component.ngOnChanges();
-
-    let expected = [
-      'v20.17.0',
-      'v18.20.4',
-      'v16.20.2',
-      'v14.21.3',
-      'v12.22.12',
-      'v10.24.1',
-      'v8.17.0'
-    ];
-    expect(component['_ltsVersions']).toStrictEqual(expected);
-  });
 });
