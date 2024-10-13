9eb3ed4fbba0dc71b9df9f11b22df27e0841c514
Sion Pixley <sionpixley@gmail.com>
2024-10-13 09:55:28 -0500

    Moving some recent logic out of utilities package. The utilities package is meant for reusable logic

--- internal/utilities/utils.go
+++ internal/utilities/utils.go
@@ -1,15 +1,10 @@
 package utilities
 
 import (
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 
-	"github.com/sionpixley/PolyNode/internal"
 	"github.com/sionpixley/PolyNode/internal/constants"
 	"github.com/sionpixley/PolyNode/internal/models"
 )
@@ -45,44 +40,6 @@ func ConvertToSemanticVersion(version string) string {
 	} else {
 		return "v" + version
 	}
-}
-
-func DownloadPolyNodeFile(filename string) error {
-	fmt.Print("Downloading the latest release of PolyNode...")
-
-	client := new(http.Client)
-	request, err := http.NewRequest(http.MethodGet, "https://github.com/sionpixley/PolyNode/releases/latest/download/"+filename, nil)
-	if err != nil {
-		return err
-	}
-
-	response, err := client.Do(request)
-	if err != nil {
-		return err
-	}
-	defer response.Body.Close()
-
-	filename = internal.PolynHomeDir + internal.PathSeparator + filename
-	err = os.RemoveAll(filename)
-	if err != nil {
-		return err
-	}
-
-	file, err := os.Create(filename)
-	if err != nil {
-		return err
-	}
-
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		file.Close()
-		return err
-	}
-	// Closing the file explicitly to avoid lock errors.
-	file.Close()
-
-	fmt.Println("Done.")
-	return nil
 }
 
 func ExtractFile(source string, destination string) error {
@@ -139,135 +96,4 @@ func IsValidVersionFormat(version string) bool {
 	}
 
 	return true
-}
-
-func UpgradePolyNode(operatingSystem models.OperatingSystem, arch models.Architecture) error {
-	var guiInstalled bool
-	if _, err := os.Stat(internal.PolynHomeDir + internal.PathSeparator + "gui"); os.IsNotExist(err) {
-		guiInstalled = false
-	} else if err != nil {
-		guiInstalled = false
-	} else {
-		guiInstalled = true
-	}
-
-	var filename string
-	switch operatingSystem {
-	case constants.AIX:
-		if guiInstalled {
-			filename = "PolyNode-GUI-aix-ppc64.tar.gz"
-		} else {
-			filename = "PolyNode-aix-ppc64.tar.gz"
-		}
-	case constants.LINUX:
-		switch arch {
-		case constants.ARM64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-linux-arm64.tar.xz"
-			} else {
-				filename = "PolyNode-linux-arm64.tar.xz"
-			}
-		case constants.X64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-linux-x64.tar.xz"
-			} else {
-				filename = "PolyNode-linux-x64.tar.xz"
-			}
-		default:
-			return errors.New(constants.UNSUPPORTED_ARCH_ERROR)
-		}
-	case constants.MAC:
-		switch arch {
-		case constants.ARM64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-darwin-arm64.tar.gz"
-			} else {
-				filename = "PolyNode-darwin-arm64.tar.gz"
-			}
-		case constants.X64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-darwin-x64.tar.gz"
-			} else {
-				filename = "PolyNode-darwin-x64.tar.gz"
-			}
-		default:
-			return errors.New(constants.UNSUPPORTED_ARCH_ERROR)
-		}
-	case constants.WINDOWS:
-		switch arch {
-		case constants.ARM64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-win-arm64.zip"
-			} else {
-				filename = "PolyNode-win-arm64.zip"
-			}
-		case constants.X64:
-			if guiInstalled {
-				filename = "PolyNode-GUI-win-x64.zip"
-			} else {
-				filename = "PolyNode-win-x64.zip"
-			}
-		default:
-			return errors.New(constants.UNSUPPORTED_ARCH_ERROR)
-		}
-	default:
-		return errors.New(constants.UNSUPPORTED_OS_ERROR)
-	}
-
-	err := DownloadPolyNodeFile(filename)
-	if err != nil {
-		return err
-	}
-
-	fmt.Print("Extracting " + filename + "...")
-	filename = internal.PolynHomeDir + internal.PathSeparator + filename
-	err = ExtractFile(filename, internal.PolynHomeDir+internal.PathSeparator+"upgrade-temp")
-	if err != nil {
-		return err
-	}
-	fmt.Println("Done.")
-
-	return runUpgradeScript(operatingSystem)
-}
-
-func runUpgradeScript(operatingSystem models.OperatingSystem) error {
-	fmt.Print("Running upgrade script...")
-	if operatingSystem == constants.WINDOWS {
-		batchfilePath := internal.PolynHomeDir + internal.PathSeparator + "polyn-upgrade-temp.cmd"
-		upgradeBatchfile := `@echo off
-
-timeout /t 1 /nobreak > nul
-cd %LOCALAPPDATA%\Programs\PolyNode\upgrade-temp
-.\setup
-cd %LOCALAPPDATA%
-del %LOCALAPPDATA%\Programs\PolyNode\upgrade-temp /s /f /q > nul
-rmdir %LOCALAPPDATA%\Programs\PolyNode\upgrade-temp /s /q
-(goto) 2>nul & del "%~f0%"`
-
-		err := os.WriteFile(batchfilePath, []byte(upgradeBatchfile), 0700)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("Done.")
-		return exec.Command("cmd", "/c", "start", "/b", batchfilePath).Run()
-	} else {
-		scriptPath := internal.PolynHomeDir + internal.PathSeparator + "polyn-upgrade-temp"
-		upgradeScript := `#!/bin/sh
-
-sleep 1
-cd $HOME/.PolyNode/upgrade-temp
-./setup
-cd $HOME
-rm -rf $HOME/.PolyNode/upgrade-temp
-rm $HOME/.PolyNode/polyn-upgrade-temp`
-
-		err := os.WriteFile(scriptPath, []byte(upgradeScript), 0700)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("Done.")
-		return exec.Command(scriptPath).Run()
-	}
 }
