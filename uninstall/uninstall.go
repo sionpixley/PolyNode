9660b632ca0c6ed46a85a8e479a8ce3e9fdab105
Sion Pixley <sionpixley@gmail.com>
2024-11-08 17:20:33 -0600

    Consistent

--- uninstall/uninstall.go
+++ uninstall/uninstall.go
@@ -78,7 +78,7 @@ func removePathLinuxAndMac(home string) error {
 	} else if strings.HasSuffix(shell, "/ksh") {
 		return removePath(home, ".kshrc")
 	} else {
-		return errors.New("uninstall error: unsupported shell")
+		return errors.New("uninstall: unsupported shell")
 	}
 }
 
