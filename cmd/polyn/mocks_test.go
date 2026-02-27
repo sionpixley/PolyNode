c3c642e311f18adbf6a184b08e2d6ea1a276f189
Sion Pixley <sionpixley@gmail.com>
2026-02-26 22:04:41 -0600

    Getting rid of some warnings

--- cmd/polyn/mocks_test.go
+++ cmd/polyn/mocks_test.go
@@ -2,22 +2,22 @@ package main
 
 import "os"
 
-func isNotExistMockFalse(error) bool {
+func isNotExistMockFalse(_ error) bool {
 	return false
 }
 
-func isNotExistMockTrue(error) bool {
+func isNotExistMockTrue(_ error) bool {
 	return true
 }
 
-func readFileMockEmptyFile(string) ([]byte, error) {
+func readFileMockEmptyFile(_ string) ([]byte, error) {
 	return []byte{}, nil
 }
 
-func readFileMockFakeData(string) ([]byte, error) {
+func readFileMockFakeData(_ string) ([]byte, error) {
 	return []byte("2026-02-26T12:23:11.723Z"), nil
 }
 
-func statMock(string) (os.FileInfo, error) {
+func statMock(_ string) (os.FileInfo, error) {
 	return nil, nil
 }
