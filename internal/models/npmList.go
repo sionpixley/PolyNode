1be6a276cdc1f0e5ccbc19cb8c2261892529ccde
Sion Pixley <sionpixley@gmail.com>
2026-04-04 08:31:08 -0500

    More efficient memory usage by change the order of the fields

--- internal/models/npmList.go
+++ internal/models/npmList.go
@@ -1,11 +1,11 @@
 package models
 
 type NPMPackage struct {
-	Overridden bool   `json:"overridden"`
 	Version    string `json:"version"`
+	Overridden bool   `json:"overridden"`
 }
 
 type NPMList struct {
-	Name         string                `json:"name"`
 	Dependencies map[string]NPMPackage `json:"dependencies"`
+	Name         string                `json:"name"`
 }
